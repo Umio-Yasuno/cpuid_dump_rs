b7ea76fb66c7031e2fb6810120cc3e907bf8d3fd
Umio Yasuno <53935716+Umio-Yasuno@users.noreply.github.com>
2021-05-08 21:46:47 +0900

    WIP

--- src/lib.rs
+++ src/lib.rs
@@ -31,52 +31,6 @@ pub fn get_processor_name() -> String {
     return String::from_utf8(name).unwrap();
 }
 
-/*
-pub fn cache_info() {
-    println!("Cache info");
-    line();
-
-    let mut a: [u32; 4] = [0; 4];
-
-    for i in 0..=4 {
-        unsafe {
-            asm!("cpuid",
-                inlateout("eax") _AX + 0x1d => a[0],
-                lateout("ebx") a[1],
-                inlateout("ecx") i => a[2],
-                lateout("edx") a[3]
-            );
-        }
-        let cache_type =
-            match a[0] & 0b11111 {
-                1 => "Data",
-                2 => "Inst",
-                3 => "Unified",
-                _    => "unknown",
-        };
-        let cache_line = (a[1] & 0xfff) + 1;
-        let cache_way  = (a[1] >> 22) + 1;
-        let cache_set  = a[2] + 1;
-
-        let cache_size = cache_line * cache_way * cache_set;
-        let cache_size_str =
-            if cache_size < 1000_000 {
-                format!("{} KiB,", cache_size / (1 << 10))
-            } else if cache_size < 1000_000_000 {
-                format!("{} MiB,", cache_size / (1 << 20))
-            } else {
-                format!("{} B,", cache_size)
-            };
-
-        println!("{}: eax=0x{:08X} ebx=0x{:08X} ecx=0x{:08X} edx=0x{:08X}",
-            i, a[0], a[1], a[2], a[3]);
-        println!("  Level{0} {1:7} cache: {5:8} {2:3}-byte line size, {3:2}-way {4}-sets",
-            a[0] >> 5 & 0b111, cache_type, cache_line, cache_way, cache_set, cache_size_str);
-    }
-    println!();
-}
-*/
-
 fn amd_cache_way(ecx: u32) -> u32 {
     let mut a: u32;
 
@@ -191,35 +145,6 @@ impl FamModStep {
     }
 }
 
-/*
-pub fn fam_mod_step() {
-    let mut a: u32;
-
-    unsafe {
-        asm!("cpuid",
-            inout("eax") _AX + 0x1 => a,
-            lateout("ebx") _,
-            lateout("ecx") _,
-            lateout("edx") _,
-        );
-    }
-
-    let base_fam = (a >> 8) & 0xf;
-    let ext_fam  = (a >> 20) & 0xff;
-    let base_mod = (a >> 4) & 0xf;
-    let ext_mod  = (a >> 12) & 0xf0;
-    let step     = a & 0xf;
-
-    println!("Base.Family: 0x{0:X} ({0})", base_fam);
-    println!("Ext.Family:  0x{0:X} ({0})", ext_fam);
-    println!("Base.Model:  0x{0:X} ({0})", base_mod);
-    println!("Ext.Model:   0x{0:X} ({0})", ext_mod);
-    println!("Family: 0x{0:X} ({0}), Model: 0x{1:X} ({1}), Stepping: {2}",
-        base_fam + ext_fam, base_mod + ext_mod, step);
-    println!();
-}
-*/
-
 pub struct CpuCoreCount {
     pub has_htt:            bool,
     pub phy_core:           u32,
@@ -267,53 +192,6 @@ impl CpuCoreCount {
         }
     }
 }
-
-/*
-pub fn core_count() {
-    println!("Core Count");
-    line();
-
-    let mut a: [u32; 4] = [0; 4];
-    let mut b: [u32; 4] = [0; 4];
-
-    unsafe {
-        asm!("cpuid",
-            inlateout("eax") 0x1 => a[0],
-            lateout("ebx") a[1],
-            lateout("ecx") _,
-            lateout("edx") a[3],
-        );
-        asm!("cpuid",
-            in("eax") _AX + 0x1e,
-            lateout("ebx") b[1],
-            lateout("ecx") b[2],
-            lateout("edx") _,
-        );
-    }
-
-    let has_htt             = ((a[3] >> 28) & 0x1) == 1;
-    let logical_core        = (a[1] >> 16) & 0xff;
-    let threads_per_core    = ((b[1] >> 8) & 0xff) + 1;
-    let phy_core            = if has_htt && 1 < threads_per_core {
-                                logical_core / threads_per_core
-                            } else if has_htt {
-                                logical_core / 2
-                            } else {
-                                logical_core
-                            };
-    let core_id             = b[1] & 0xff;
-
-    println!("0x{:08x}: eax=0x{:08X} ebx=0x{:08X} ecx=0x{:08X} edx=0x{:08X}",
-        0x1, a[0], a[1], a[2], a[3]);
-    println!("0x{:08x}: eax=0x{:08X} ebx=0x{:08X} ecx=0x{:08X} edx=0x{:08X}",
-        _AX + 0x1e, b[0], b[1], b[2], b[3]);
-    println!("HTT/SMT: {}", has_htt);
-    println!("CoreID: {}", core_id);
-    println!("{}-Core/{}-Thread", phy_core, logical_core);
-    println!("Threads per Core: {}", threads_per_core);
-    println!();
-}
-*/
 
 pub fn get_vendor_name() -> String {
     let mut a: [u32; 4] = [0; 4];
