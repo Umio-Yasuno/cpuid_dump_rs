82b67b9b4344f2eba605e90759bdd1e8ba2c3ed4
Umio Yasuno <53935716+Umio-Yasuno@users.noreply.github.com>
2022-10-12 16:00:20 +0900

    update codename_amd

--- lib/codename/codename_amd.rs
+++ lib/codename/codename_amd.rs
@@ -3,37 +3,37 @@ use crate::ProcInfo;
 impl ProcInfo {
 pub(super) fn fam17h(m: u32, s: u32) -> Self {
     match m {
-        // Zen
+        /* Zen */
         0x01 => match s {
             0x01 => Self::info("Summit Ridge (B1)", "Zen", "14 nm"),
             0x02 => Self::info("Naples (B2)", "Zen", "14 nm"),
             _ => Self::info("Zen", "Zen", ""),
         },
-        0x20 => Self::info(&format!("Raven2 [Dali/Pollock]{}", match s {
+        0x20 => Self::info(&["Raven2 [Dali/Pollock]", match s {
             0x1 => " (A1)",
             _ => "",
-        }), "Zen", "14 nm"),
+        }].concat(), "Zen", "14 nm"),
 
-        // Zen+
-        0x08 => Self::info(&format!("Pinnacle Ridge{}", match s {
+        /* Zen+ */
+        0x08 => Self::info(&["Pinnacle Ridge", match s {
             0x2 => " (B2)",
             _ => "",
-        }), "Zen+", "12 nm"),
-        0x18 => Self::info(&format!("Picasso{}", match s {
+        }].concat(), "Zen+", "12 nm"),
+        0x18 => Self::info(&["Picasso", match s {
             0x1 => " (B1)",
             _ => "",
-        }), "Zen+", "12 nm"),
+        }].concat(), "Zen+", "12 nm"),
 
-        // Zen 2
+        /* Zen 2 */
         0x31 => Self::info("Rome", "Zen 2", "7 nm"),
-        0x60 => Self::info(&format!("Renoir{}", match s {
+        0x60 => Self::info(&["Renoir", match s {
             0x1 => " (A1)",
             _ => "",
-        }), "Zen 2", "7 nm"),
+        }].concat(), "Zen 2", "7 nm"),
         0x68 => Self::info("Lucienne", "Zen 2", "7 nm"),
         0x71 => Self::info("Matisse", "Zen 2", "7 nm"),
         0x90 => Self::info("VanGogh", "Zen 2", "7 nm"),
-        0xA0 => Self::info("Mendocino", "Zen 2", "6 nm"),
+        0xA0..=0xAF => Self::info("Mendocino", "Zen 2", "6 nm"),
 
         _ => Self::info("Fam17h", "Zen", ""),
     }
@@ -41,17 +41,20 @@ pub(super) fn fam17h(m: u32, s: u32) -> Self {
 
 pub(super) fn fam19h(m: u32, s: u32) -> Self {
     match m {
-        0x01 => Self::info(&format!("Milan{}", match s {
+        /* Zen 3 */
+        0x01 => Self::info(&["Milan", match s {
             0x1 => " (B0)", // EPYC 7003
             0x2 => " (B1)", // EPYC 7003 3D V-Cache
             _ => "",
-        }), "Zen 3", "7 nm"),
+        }].concat(), "Zen 3", "7 nm"),
         0x21 => Self::info("Vermeer", "Zen 3", "7 nm"),
         /* https://www.openmp.org/wp-content/uploads/ecp_sollve_openmp_monthly.offload_perf_ana_craypat.marcus.hpe_.26aug2022.v2.pdf */
         0x30 => Self::info("Trento", "Zen 3", "7 nm"),
         // 0x44: Rembrandt
-        0x40..=0x4F => Self::info("Rembrandt", "Zen 3+", "6nm"),
-        0x50..=0x5F => Self::info("Cezanne (A1)", "Zen 3", "7 nm"),
+        0x40..=0x4F => Self::info("Rembrandt", "Zen 3+", "6 nm"),
+        0x50..=0x5F => Self::info("Cezanne", "Zen 3", "7 nm"),
+
+        /* Zen 4 */
         0x60..=0x6F => Self::info("Phoenix", "Zen 4", ""),
         0x70..=0x7F => Self::info("Raphael", "Zen 4", "5 nm"),
 
