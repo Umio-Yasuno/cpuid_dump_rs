129a9627f958e26aecb96c6e45c43973b1abcc7e
Umio Yasuno <53935716+Umio-Yasuno@users.noreply.github.com>
2022-11-01 00:15:40 +0900

    cleanup parse_util

--- src/parse/parse_util.rs
+++ src/parse/parse_util.rs
@@ -19,39 +19,22 @@ macro_rules! lnpad {
     };
 }
 
-pub(crate) struct Reg(u32);
-impl Reg {
-    pub(crate) fn new(reg: u32) -> Self {
-        Self(reg)
+fn u32_to_bool_array(reg: u32) -> [bool; 32] {
+    let mut flags = [false; 32];
+
+    for (pos, flag) in flags.iter_mut().enumerate() {
+        *flag = ((reg >> pos) & 0b1) != 0;
     }
 
-    fn to_bit_array(&self) -> [u8; 32] {
-        let mut array = [0u8; 32];
-
-        for (pos, v) in array.iter_mut().enumerate() {
-            *v = ((self.0 >> pos) & 1) as u8;
-        }
-
-        array
-    }
-
-    pub(crate) fn to_bool_array(&self) -> [bool; 32] {
-        let mut array = [false; 32];
-
-        for (bit, flag) in self.to_bit_array().iter().zip(array.iter_mut()) {
-            *flag = *bit != 0;
-        }
-
-        array
-    }
+    flags
 }
 
 pub(crate) fn str_detect_ftr(reg: u32, ftr_str: &[&str]) -> Vec<String> {
-    let reg = Reg::new(reg).to_bool_array();
+    let flags = u32_to_bool_array(reg);
     let mut buff: Vec<String> = Vec::with_capacity(32);
 
-    for (r, ftr) in reg.iter().zip(ftr_str.iter()) {
-        if *r && !ftr.is_empty() {
+    for (flag, ftr) in flags.iter().zip(ftr_str.iter()) {
+        if *flag && !ftr.is_empty() {
             buff.push(ftr.to_string());
         }
     }
@@ -80,20 +63,3 @@ pub(crate) fn align_mold_ftr(ftrs: &[String]) -> String {
 
     mold
 }
-
-/*
-pub(crate) fn ftr_variant_expand(base_name: &str, flag_str: &[(bool, &str)]) -> String {
-    let mut base = format!("{base_name}{{");
-
-    for (flag, name) in flag_str {
-        if *flag {
-            write!(base, "{name},").unwrap();
-        }
-    }
-    
-    base.pop();
-    write!(base, "}}").unwrap();
-
-    return base;
-}
-*/
